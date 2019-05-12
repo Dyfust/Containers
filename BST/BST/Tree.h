3fc11313578102b17d07da251860f28f109a636b
Reyhan Rishard <s182355@students.aie.edu.au>
2019-05-12 20:58:25 +1000

    BST Remove function completed.

--- BST/BST/Tree.h
+++ BST/BST/Tree.h
@@ -7,6 +7,41 @@ class Tree
 private:
 	Node<T>* m_root_node = nullptr;
 
+	void remove_leaf(Node<T>* leaf)
+	{
+		Node<T>* parent_node = leaf->m_parent;
+
+		// Find and change the parent's child pointer which pointed to this leaf to nullptr before deleting the leaf.
+		if (parent_node->m_left == leaf)
+		{
+			parent_node->m_left = nullptr;
+		}
+		else
+		{
+			parent_node->m_right = nullptr;
+		}
+
+		delete leaf;
+	}
+
+	void remove_node_with_one_child(Node<T>* node, Node<T>* child)
+	{
+		Node<T>* parent_node = node->m_parent;
+		child->m_parent = parent_node;
+
+		// Find and change the parent's child pointer which pointed to this node to the node's child before deleting the node.
+		if (parent_node->m_left == node)
+		{
+			parent_node->m_left = child;
+		}
+		else if (parent_node->m_right == node)
+		{
+			parent_node->m_right = child;
+		}
+
+		delete node;
+	}
+
 public:
 	Tree<T>() {}
 	~Tree() {}
@@ -65,17 +100,20 @@ public:
 	bool find(T& data)
 	{
 		Node<T>* current_node = m_root_node;
-
 		bool found = false;
 
 		while (!found)
 		{
+			// Traverse down the tree till the node is found or there's no nodes.
 			if (current_node != nullptr)
 			{
+				// Go down to the tree to the left if the data is smaller than the node we're currently inspecting.
 				if (data < *current_node->m_data)
 				{ current_node = current_node->m_left; }
+				// Go down to the right if it's bigger.
 				else if (data > *current_node->m_data)
 				{ current_node = current_node->m_right; }
+				// Otherwise change "found" to true so we can return out of this function in the next iteration.
 				else 
 				{ found = true; }
 			}
@@ -86,5 +124,92 @@ public:
 		}
 
 		return found;
+	}
+
+	bool remove(const T& data)
+	{
+		Node<T>* target_node = m_root_node;
+		bool found = false;
+
+		// Find the node & point to it. Return false if the node cannot be found.
+		while (!found)
+		{
+			if (target_node != nullptr)
+			{
+				if (data < *target_node->m_data)
+				{
+					target_node = target_node->m_left;
+				}
+				else if (data > * target_node->m_data)
+				{
+					target_node = target_node->m_right;
+				}
+				else
+				{
+					found = true;
+				}
+			}
+			else
+			{
+				return found;
+			}
+		}
+
+		// The code that actually deletes the node.
+		bool removed = false;
+
+		// Situation #1: target node has no children.
+		if (target_node->m_left == nullptr && target_node->m_right == nullptr) 
+		{
+			remove_leaf(target_node);
+			removed = true;
+		}
+		// Situation #2: target node has one child.
+		else if (target_node->m_left != nullptr && target_node->m_right == nullptr) 
+		{
+			remove_node_with_one_child(target_node, target_node->m_left);
+			removed = true;
+		}
+		else if (target_node->m_left == nullptr && target_node->m_right != nullptr)
+		{
+			remove_node_with_one_child(target_node, target_node->m_right);
+			removed = true;
+		}
+		// Siutation #3: target node has 2 children.
+		else
+		{
+			// Find the smallest node that's bigger than the node that's being deleted.
+			Node<T>* node_to_swap_with = target_node->m_right;
+
+			while (node_to_swap_with->m_left != nullptr) 
+			{
+				node_to_swap_with = node_to_swap_with->m_left;
+			}
+
+			// Swap the data in them around.
+			target_node->m_data = node_to_swap_with->m_data;
+
+			// Point the target_node pointer to the node we just made the swap with and delete that instead.
+			target_node = node_to_swap_with;
+
+			// Situation #1: noo children.
+			if (target_node->m_left == nullptr && target_node->m_right == nullptr)
+			{
+				remove_leaf(target_node);
+			}
+			// Situation #2: 1 child.
+			else if (target_node->m_left != nullptr && target_node->m_right == nullptr)
+			{
+				remove_node_with_one_child(target_node, target_node->m_left);
+			}
+			else if (target_node->m_left == nullptr && target_node->m_right != nullptr)
+			{
+				remove_node_with_one_child(target_node, target_node->m_right);
+			}
+
+			removed = true;
+		}
+
+		return removed;
 	}
 };
--- BST/BST/main.cpp
+++ BST/BST/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include "Tree.h"
+#include <crtdbg.h>
 
 int main()
 {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 	Tree<int> m_bst;
 
 	int num = 10;
 	int num2 = 9;
 	int num3 = 15;
 	int num4 = 21;
-	int num5 = 18;
+	int num5 = 13;
+	int num6 = 24;
 
 	bool found = true;
 
@@ -17,8 +20,12 @@ int main()
 	m_bst.insert(num2);
 	m_bst.insert(num3);
 	m_bst.insert(num4);
+	m_bst.insert(num5);
+	m_bst.insert(num6);
 
 	found = m_bst.find(num5);
+
+	m_bst.remove(num3);
 
 	return 0;
 }
